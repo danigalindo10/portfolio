06b742047918a4efcb0e42916f747be1d76b9af5
Jean Bulambo <jeanbulambo4@gmail.com>
2022-06-20 21:54:06 +0200

    First commit add js file Riziki

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburgerButton = document.getElementById('menu-btn');
+
+hamburgerButton.addEventListener('click', () => {
+  document.getElementById('lead').className += ' display-menu';
+  document.getElementsByClassName('logo')[0].className += ' display-menu';
+  document.getElementById('menu').className += ' display-menu';
+  document.getElementsByClassName('nav-container')[0].classList.add('display-menu');
+  document.getElementsByClassName('categories')[0].classList.add('display-menu');
+  document.getElementsByClassName('close')[0].classList.add('display-menu');
+  document.getElementsByClassName('header-content')[0].classList.add('display-menu');
+  document.getElementsByTagName('body')[0].classList.add('display-menu');
+
+  for (let i = 0; i < document.getElementsByClassName('category').length; i++) {
+    document.getElementsByClassName('category')[i].classList.add('display-menu');
+  }
+});
